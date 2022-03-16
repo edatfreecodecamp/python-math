545781fca54e58b52ba4bb056100ef6611138f18
edatfreecodecamp <101264869+edatfreecodecamp@users.noreply.github.com>
2022-03-16 15:52:02 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-math',
+    version='1.0',
+    packages=find_packages(include=['python-math', 'python-math.math-code-test-a'])
+)
